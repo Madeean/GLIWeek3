5a89092b82125a7a0142f74d645c57fa2ad03ff3
MadeReihan <reihanmade@gmail.com>
2023-03-10 21:55:05 +0700

    create Model,Fragment, tabLayout, viewPager

--- app/src/main/java/com/madeean/weeklyproject3no1/MainActivity.kt
+++ app/src/main/java/com/madeean/weeklyproject3no1/MainActivity.kt
@@ -2,10 +2,32 @@ package com.madeean.weeklyproject3no1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var tabLayout: TabLayout
+    private lateinit var viewPager: ViewPager2
+    private val TAB_TITLES = arrayOf(
+        "Semua",
+        "Makanan",
+        "Minuman",
+        "Elektronik",
+        "Pakaian"
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tabLayout = findViewById(R.id.tab_layout)
+        viewPager = findViewById(R.id.view_pager)
+
+        val tabAdapter = TabAdapter(this)
+        viewPager.adapter = tabAdapter
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = TAB_TITLES[position]
+        }.attach()
+
     }
 }
